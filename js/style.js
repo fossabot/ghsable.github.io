9dfa2bc95c7b39339d0243ed5ac1ed2cc4542856
ghsable <sn.sable005@gmail.com>
2019-04-16 00:56:54 +0900

    fix

---
+++ js/style.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('#fadein').click(function(){
+    $('.articleDetailBodyList').fadeIn(2000);  //2.0秒かけてフェードイン
+  });
+  $('#fadeout').click(function(){
+    $('.articleDetailBodyList').fadeOut(2000); //2.0秒かけてフェードアウト
+  });
+});

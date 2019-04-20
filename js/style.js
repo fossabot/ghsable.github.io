a5faadd54edaf41f2fc82c9b960be1f13ee1f726
ghsable <sn.sable005@gmail.com>
2019-04-21 01:58:46 +0900

    fix

--- js/style.js
+++ js/style.js
@@ -1,8 +1,9 @@
-$(function() {
-  $('#fadein').click(function(){
-    $('.articleDetailBodyList').fadeIn(2000);  //2.0秒かけてフェードイン
-  });
-  $('#fadeout').click(function(){
-    $('.articleDetailBodyList').fadeOut(2000); //2.0秒かけてフェードアウト
-  });
-});
+// --- フェードイン/アウト操作
+// 変数定義
+var btn_power = document.getElementById('btn_power');
+var box = document.getElementById('box2-fade');
+
+// ボタンクリック時に.is-showクラス付与
+btn_power.onclick = function () {
+  box.classList.toggle('is-show')
+}

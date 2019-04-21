2195da002cc084ed1bc1da5545eb15fada17dc38
ghsable <sn.sable005@gmail.com>
2019-04-21 15:38:42 +0900

    fix

--- js/style.js
+++ js/style.js
@@ -1,7 +1,7 @@
 // --- フェードイン/アウト操作
 // 変数定義
 var btn_power = document.getElementById('btn_power');
-var box = document.getElementById('box2-fade');
+var box = document.getElementById('box2-content');
 
 // ボタンクリック時に.is-showクラス付与
 btn_power.onclick = function () {

45cac96aeee4ab47e9c35bab7c40cda6d40983dc
ghsable <sn.sable005@gmail.com>
2019-04-21 20:07:02 +0900

    fix

--- js/style.js
+++ js/style.js
@@ -7,3 +7,12 @@ var box = document.getElementById('box2-content');
 btn_power.onclick = function () {
   box.classList.toggle('is-show')
 }
+
+// --- メッセージ表示
+// 変数定義
+var btn_home = document.getElementById('btn_home');
+
+// ボタンクリック時にアラート表示
+btn_home.onclick = function () {
+  alert('隴西の李徴「うわぁああぁああああぁあaーーーーー」');
+}

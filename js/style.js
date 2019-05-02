59f58a6d1b9491c520af8e84860caf984a5e887b
ghsable <sn.sable005@gmail.com>
2019-05-03 01:27:31 +0900

    fix all

--- js/style.js
+++ js/style.js
@@ -1,16 +1,16 @@
 // --- フェードイン/アウト操作
 // 変数定義
 var btn_power = document.getElementById('btn_power');
-var box = document.getElementById('box2-content');
+var box = document.getElementById('articleDetailBody');
 
 // ボタンクリック時に.is-showクラス付与
 btn_power.onclick = function () {
   box.classList.toggle('is-show')
 }
 
-// --- メッセージ表示+リンク誘導
+// --- メッセージ表示
 // 変数定義
-var btnl_home = document.getElementById('btnl_home');
+var btnl_home = document.getElementById('btn_home');
 
 // ボタンクリック時にアラート表示
 // 熱帯雨林の植林活動の支援をさせる
@@ -20,10 +20,8 @@ btnl_home.onclick = function () {
 
 // --- 波紋エフェクト
 // sirxemic/jquery.ripples(https://github.com/sirxemic/jquery.ripples)
-$(document).ready(function() {
-  $('.articleDetailBodyList').ripples({  //波紋をつける要素を指定
-  resolution: 400,                       //波紋が広がる速さ
-  dropRadius: 8,                         //波紋の大きさ
-  perturbance: 0.8                       //波紋の揺れの量
-  });
+$('#articleDetail').ripples({      // 波紋をつける要素を指定
+  resolution: 512,                 // 波紋が広がる速さ
+  dropRadius: 20,                  // 波紋の大きさ
+  perturbance: 0.04                // 波紋の揺れの量
 });

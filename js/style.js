15aed2872abcf73ec3c178fb25ea7f175046231e
ghsable <sn.sable005@gmail.com>
2019-05-03 01:51:19 +0900

    fix

--- js/style.js
+++ js/style.js
@@ -1,6 +1,6 @@
 // --- フェードイン/アウト操作
 // 変数定義
-var btn_power = document.getElementById('btn_power');
+var btn_power = document.getElementById('input_power');
 var box = document.getElementById('articleDetailBody');
 
 // ボタンクリック時に.is-showクラス付与
@@ -10,11 +10,11 @@ btn_power.onclick = function () {
 
 // --- メッセージ表示
 // 変数定義
-var btnl_home = document.getElementById('btn_home');
+var btn_home = document.getElementById('label_home');
 
 // ボタンクリック時にアラート表示
 // 熱帯雨林の植林活動の支援をさせる
-btnl_home.onclick = function () {
+btn_home.onclick = function () {
   alert('隴西の李徴「うわぁああぁーーー!!」');
 }
 

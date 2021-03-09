143cb36f9de1ea84d445aa022bee5b7465bef9e6
suna <sn.sable005@gmail.com>
2021-03-09 14:21:10 +0900

    fix style.js

--- js/style.js
+++ js/style.js
@@ -2,20 +2,20 @@
 // --- luisvinicius167/ityped(https://github.com/luisvinicius167/ityped)
 ityped.init(document.querySelector("#ityped"), {
   // required - for now, only accepting texts
-    strings: ['Welcome to my home.',
-              'My name is suna.',
-              'I am a Lifehacker.',
-              'I want to do something fun! XD'],
-    typeSpeed: 20,     //タイピング速度
-    backSpeed: 5,      //バックスペース速度
-    startDelay: 1000,  //アニメーション開始ディレイタイム
-    backDelay: 1000,   //バックスペース開始ディレイタイム
-    loop: true,        //ループ有無
-    showCursor: true,  //カーソル表示
-    cursorChar: "|",   //カーソルキャラクタ
-    // optional callback called once the last string has been typed
-    onFinished: function(){},
-}
+  strings: ['Welcome to my home.',
+            'My name is suna.',
+            'I am a Lifehacker.',
+            'I want to do something fun! XD'],
+  typeSpeed: 20,     //タイピング速度
+  backSpeed: 5,      //バックスペース速度
+  startDelay: 1000,  //アニメーション開始ディレイタイム
+  backDelay: 1000,   //バックスペース開始ディレイタイム
+  loop: true,        //ループ有無
+  showCursor: true,  //カーソル表示
+  cursorChar: "|",   //カーソルキャラクタ
+  // optional callback called once the last string has been typed
+  onFinished: function(){},
+})
 
 // --- フェードイン/アウト操作(CSS実装)
 // 変数定義
@@ -39,8 +39,8 @@ btn_home.onclick = function () {
 
 // --- 波紋エフェクト
 // --- sirxemic/jquery.ripples(https://github.com/sirxemic/jquery.ripples)
-$('#articleDetail #articleDetailBody .articleDetailBodyList').ripples({      // 波紋をつける要素を指定
-//  resolution: 512,                 // 波紋が広がる速さ
-//  dropRadius: 20,                  // 波紋の大きさ
-//  perturbance: 0.04                // 波紋の揺れの量
+$('#articleDetail #articleDetailBody .articleDetailBodyList').ripples({  // 波紋をつける要素を指定
+  //resolution: 512,   // 波紋が広がる速さ
+  //dropRadius: 20,    // 波紋の大きさ
+  //perturbance: 0.04  // 波紋の揺れの量
 });
